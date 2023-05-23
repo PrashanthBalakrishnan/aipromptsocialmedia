24eef1dffc4dc7b510bed975aafaec826b56b3b0
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-05-23 12:41:28 -0400

    added secret for next-auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -48,6 +48,11 @@ export const authOptions = {
             }
         },
     },
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
 }
 
 const handler = NextAuth(authOptions)
