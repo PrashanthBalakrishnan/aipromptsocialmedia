aa969fb302c1417bc059bb5ac1656ef2c8bddd1c
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-06-22 22:37:59 -0400

    fixed goolge auth for now

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,9 +6,9 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
     const [providers, setProviders] = useState(null)
+
     const [toggleDropdown, setToggleDropdown] = useState(false)
     const { data: session } = useSession()
-    console.log(Object.values(providers))
     useEffect(() => {
         const setUpProviders = async () => {
             const response = await getProviders()
@@ -16,7 +16,6 @@ const Nav = () => {
         }
         setUpProviders()
     }, [])
-
     return (
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href="/" className="flex gap-2 flex-center">
@@ -63,8 +62,8 @@ const Nav = () => {
                             Object.values(providers).map((provider) => (
                                 <button
                                     type="button"
-                                    key={provider?.name}
-                                    onClick={() => signIn(provider?.id)}
+                                    key={1}
+                                    onClick={() => signIn('google')}
                                     className="black_btn"
                                 >
                                     Sign In
@@ -122,8 +121,8 @@ const Nav = () => {
                             Object.values(providers).map((provider) => (
                                 <button
                                     type="button"
-                                    key={provider.name}
-                                    onClick={() => signIn(provider.id)}
+                                    key={1}
+                                    onClick={() => signIn('google')}
                                     className="black_btn"
                                 >
                                     Sign In
