1e34a4ef7cbd4edf82dc611e0affa3afe66b6880
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-06-23 08:33:15 -0400

    json changed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+        swcPlugins: [['next-superjson-plugin', {}]],
+    },
     images: {
-        domains: ['lh3.googleusercontent.com'],
+        domains: [
+            'res.cloudinary.com',
+            'avatars.githubusercontent.com',
+            'lh3.googleusercontent.com',
+        ],
     },
 }
 
